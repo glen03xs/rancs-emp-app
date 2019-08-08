dc97c9cd7edac346b703bc87d74c1ff7d226bfae
Glen S Nepomuceno <glen03xs@gmail.com>
2019-08-08 19:34:47 +0800

    started creating routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Connect Database
+connectDB();
 
-const PORT = process.env.PORT || 4000
+app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
