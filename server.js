8b2403b784679f4e410bebdcae2503c4f8adcd04
Glen S Nepomuceno <glen03xs@gmail.com>
2020-09-19 17:46:25 +0800

    update files for renew project

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
 
 const app = express();
 
-// Connect Database
-connectDB();
+app.get('/', (req, res) =>
+	res.json({
+		msg: 'Setup is good',
+	})
+);
 
-app.get("/", (req, res) => res.send("API Running"));
+const PORT = process.env.PORT || 5000;
 
-// Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
